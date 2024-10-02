3a1ed299144c857a929477b7c4cf151cdbd90d16
Christopher Biscardi <chris@christopherbiscardi.com>
2024-10-01 22:16:28 -0700

    basic camera

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use bevy::{
     prelude::*,
     render::{
         mesh::VertexAttributeValues,
+        render_asset::RenderAssetUsages,
+        render_resource::{
+            Extent3d, TextureDimension, TextureFormat,
+        },
         settings::{
             RenderCreation, WgpuFeatures, WgpuSettings,
         },
@@ -35,7 +39,11 @@ fn main() {
             PanOrbitCameraPlugin,
         ))
         .add_systems(Startup, startup)
-        .add_systems(Update, toggle_wireframe)
+        .add_systems(
+            Update,
+            (toggle_wireframe, sync_camera_to_ship),
+        )
+        .add_systems(Update, control_ship)
         .run();
 }
 
@@ -43,7 +51,15 @@ fn startup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
 ) {
+    let debug_material = materials.add(StandardMaterial {
+        base_color_texture: Some(
+            images.add(uv_debug_texture()),
+        ),
+        ..default()
+    });
+
     commands.spawn((
         Camera3dBundle {
             transform: Transform::from_xyz(0.0, 20., 75.0)
@@ -51,6 +67,17 @@ fn startup(
             ..default()
         },
         PanOrbitCamera::default(),
+        ShipCam,
+    ));
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::default()),
+            material: debug_material.clone(),
+            transform: Transform::from_xyz(0., 10., 0.),
+            ..default()
+        },
+        Ship,
     ));
 
     let terrain_height = 70.;
@@ -156,4 +183,84 @@ fn toggle_wireframe(
             commands.entity(terrain).remove::<Wireframe>();
         }
     }
+}
+
+#[derive(Component)]
+struct Ship;
+
+#[derive(Component)]
+struct ShipCam;
+
+fn control_ship(
+    input: Res<ButtonInput<KeyCode>>,
+    mut ships: Query<&mut Transform, With<Ship>>,
+) {
+    let mut direction = Vec2::new(0., 0.);
+    if input.pressed(KeyCode::KeyW) {
+        direction.y += 1.;
+    }
+    if input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.;
+    }
+    if input.pressed(KeyCode::KeyA) {
+        direction.x += 1.;
+    }
+    if input.pressed(KeyCode::KeyD) {
+        direction.x -= 1.;
+    }
+    for mut ship in &mut ships {
+        ship.translation.x += direction.x * 1.;
+        ship.translation.z += direction.y * 5.;
+    }
+}
+
+fn sync_camera_to_ship(
+    ships: Query<
+        &Transform,
+        (With<Ship>, Without<ShipCam>),
+    >,
+    mut camera: Query<&mut PanOrbitCamera, With<ShipCam>>,
+) {
+    let Ok(ship) = ships.get_single() else {
+        return;
+    };
+    let mut orbit = camera.single_mut();
+
+    orbit.target_focus = Vec3::new(
+        ship.translation.x,
+        ship.translation.y,
+        ship.translation.z,
+    );
+}
+
+fn uv_debug_texture() -> Image {
+    const TEXTURE_SIZE: usize = 8;
+
+    let mut palette: [u8; 32] = [
+        255, 102, 159, 255, 255, 159, 102, 255, 236, 255,
+        102, 255, 121, 255, 102, 255, 102, 255, 198, 255,
+        102, 198, 255, 255, 121, 102, 255, 255, 236, 102,
+        255, 255,
+    ];
+
+    let mut texture_data =
+        [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+    for y in 0..TEXTURE_SIZE {
+        let offset = TEXTURE_SIZE * y * 4;
+        texture_data[offset..(offset + TEXTURE_SIZE * 4)]
+            .copy_from_slice(&palette);
+        palette.rotate_right(4);
+    }
+
+    Image::new_fill(
+        Extent3d {
+            width: TEXTURE_SIZE as u32,
+            height: TEXTURE_SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD,
+    )
 }
